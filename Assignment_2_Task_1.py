f2225341be2cbbc3d59a5116c3dd5ed3045d4085
Varun Dhamankar <var24run@gmail.com>
2025-11-04 10:08:16 +0530

    Add files via upload

---
+++ Assignment_2_Task_1.py
@@ -0,0 +1,3 @@
+# To check if the entered number is even or odd.
+num = int(input("Enter a number: "))
+print("Even") if num % 2 == 0 else print("Odd")
---
+++ Assignment_2_Task_2.py
@@ -0,0 +1,5 @@
+# Sum of numbers from 1 to 50
+Sum_of_numbers = 0
+for i in range(1, 51):
+    Sum_of_numbers += i
+print(f"The sum of numbers from 1 to 50 is {Sum_of_numbers}")
